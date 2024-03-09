b4b4f28db3f207bb129f0eced9dd155e3c1792f9
Math_Amphetamine <147948392+Bruhout@users.noreply.github.com>
2024-03-10 02:38:33 +0530

    Update linked_list.h
    Added Documentation

--- linked_list.h
+++ linked_list.h
@@ -1,17 +1,86 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
 typedef struct NODE {
-    void* data;
-    struct NODE* next_node;
-}NODE;
+    void *data;
+    struct NODE *next_node;
+} NODE;
 
-
-NODE* create_node(void* data_element);
-NODE* create_list(void* data_element);
-int insert_at_beginning(NODE** first_node_ptr, void* data_element);
-int insert_at_end(NODE* first_node, void* data_element);
-int list_length(NODE* first_node);
-int insert_at_index(NODE* first_node, void* input_data, int index);
-int print_list_char(NODE* first_node);
-int print_list_int(NODE* first_node);
-void* indexing(NODE* first_node, int index);
-void concatenate(NODE* first_list, NODE* second_list);
-void delete_node(NODE** list, int index);
+NODE *create_node(void *data_element);
+/*
+Returns pointer to the node created.
+Returns NULL if memory allocation failed.
+*/
+//---------------------------------------------
+NODE *create_list(void *data_element);
+/*
+Returns pointer the first node of the list.
+The next_node attribute is set to NULL.
+Returns -1 if memory allocation error.
+*/
+//---------------------------------------------
+int insert_at_beginning(NODE **first_node_ptr, void *data_element);
+/*
+list is passed as a NODE**, a pointer to the pointer to the first node.
+This is because the NODE* needs to be passed by reference.
+Returns -2 if invalid link passed
+Returns -1 if memory allocation error
+Return 0 on successful insertion
+*/
+//---------------------------------------------
+int insert_at_end(NODE *first_node, void *data_element);
+/*
+Returns -2 if invalid linked list passed
+Returns -1 if memory allocation error
+Return 0 on successful insertion
+*/
+//---------------------------------------------
+int list_length(NODE *list_head);
+/*
+Returns -2 if invalid linked list passed
+Otherwise returns integer count of the nodes in the list
+*/
+//---------------------------------------------
+int insert_at_index(NODE *list_head, void *data_element, int index);
+/*
+Returns -1 in case of memory allocation error
+Returns -2 if invalid linked list is passed
+Returns -3 if invalid index is passed. (index<0 or index>=list_length(list))
+Otherwise returns 0 on successful insersertion. 
+*/
+//---------------------------------------------
+int print_list_char(NODE *head_node);
+int print_list_int(NODE *first_node);
+/*
+print_list_type, The function must be defined for each type that you wish to support printing.
+By default, the library only includes char* and int.
+Returns -2 if invalid linked list is passed
+Otherwise, returns 0 if successfully executed.
+*/
+//---------------------------------------------
+void *indexing(NODE *first_node, int index);
+/*
+Since the list only stores void*, indexing will return a pointer to the data.
+This pointer must be dereferenced to access the data.
+This is done so that the same indexing function can be used for all data types.
+Returns NULL if index is invalid. (index<0 or index>=list_length(list))
+Returns NULL if invalid linked list is passed
+*/
+//---------------------------------------------
+void concatenate(NODE *first_list, NODE *second_list);
+/*
+No return value from this function.
+The second_list is placed at the end of the first list.
+The contatenated list is stored in the first_list pointer.
+*/
+//---------------------------------------------
+void delete_node(NODE **list, int index);
+/*
+list is passed as a NODE**, a pointer to the pointer to the first node.
+This is because the NODE* needs to be passed by reference.
+No return value from this function.
+Execution terminated if invalid linked list passed
+Execution terminated if invalid index passed
+*/
+//---------------------------------------------
+#endif /* LINKED_LIST_H */
